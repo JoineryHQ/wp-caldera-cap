9d94c6611b2bc9ee7511e422470e162e24496d83
Allen Shaw <allen@JoineryHQ.com>
2024-11-21 14:31:08 -0600

    Initial commit, per boilerplate from https://wppb.me/.

---
+++ includes/class-wp-caldera-cap-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://joineryhq.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Caldera_Cap
+ * @subpackage Wp_Caldera_Cap/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Caldera_Cap
+ * @subpackage Wp_Caldera_Cap/includes
+ * @author     Allen Shaw <allen@joineryhq.com>
+ */
+class Wp_Caldera_Cap_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-caldera-cap-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://joineryhq.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Caldera_Cap
+ * @subpackage Wp_Caldera_Cap/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Caldera_Cap
+ * @subpackage Wp_Caldera_Cap/includes
+ * @author     Allen Shaw <allen@joineryhq.com>
+ */
+class Wp_Caldera_Cap_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
